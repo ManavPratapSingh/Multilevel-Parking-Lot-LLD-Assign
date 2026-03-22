dad6f02e5e804e86b1e3c32a3318dcbf4f4a4ab4
ManavPratapSingh-off <manav3407.gb@gmail.com>
2026-03-22 22:21:09 +0530

    initial commit

---
+++ src/main/java/com/manav/multilevel_parking_system/MultilevelParkingLotSystemApplication.java
@@ -0,0 +1,13 @@
+package com.manav.multilevel_parking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultilevelParkingLotSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultilevelParkingLotSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manav/multilevel_parking_system/MultilevelParkingLotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manav.multilevel_parking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultilevelParkingLotSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
